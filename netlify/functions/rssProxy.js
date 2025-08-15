1457ad381cdd602763b973e2a379746b826e9a56
EnCorto News <valdesa868@gmail.com>
2025-08-15 16:09:01 -0600

    Actualizar rssProxy.js

--- netlify/functions/rssProxy.js
+++ netlify/functions/rssProxy.js
@@ -3,26 +3,11 @@ const fetch = require('node-fetch');
 const REPLIT_DOMAIN = process.env.REPLIT_URL; // tu URL de Replit como secreto
 const OFFICIAL_DOMAIN = 'https://mx.encortonews.qzz.io';
 
-function escapeXml(unsafe) {
-  return unsafe.replace(/[<>&'"]/g, function (c) {
-    switch (c) {
-      case '<': return '&lt;';
-      case '>': return '&gt;';
-      case '&': return '&amp;';
-      case '\'': return '&apos;';
-      case '"': return '&quot;';
-    }
-  });
-}
-
 exports.handler = async function(event, context) {
   try {
     // Obtener RSS de Replit
     const response = await fetch(`${REPLIT_DOMAIN}/rss`);
     let rssText = await response.text();
-
-    // Escapar caracteres especiales
-    rssText = escapeXml(rssText);
 
     // Reemplazar URLs de Replit por dominio oficial
     const updatedRss = rssText.replace(new RegExp(REPLIT_DOMAIN, 'g'), OFFICIAL_DOMAIN);
